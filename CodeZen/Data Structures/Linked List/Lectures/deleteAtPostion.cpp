71f826448039e30ea898b9490e27ed6dee2473db
Kaustav Ghosh <teetangh@gmail.com>
2020-03-02 20:18:06 +0530

    cut short at lot of code

--- CodeZen/Data Structures/Linked List/Lectures/deleteAtPostion.cpp
+++ CodeZen/Data Structures/Linked List/Lectures/deleteAtPostion.cpp
@@ -1,52 +1,5 @@
 #include <iostream>
-class Node
-{
-public:
-    int data;
-    Node *next;
-    Node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
-    }
-};
-
-using namespace std;
-// #include "Solution.h"
-
-Node *takeinput()
-{
-    int data;
-    cin >> data;
-    Node *head = NULL, *tail = NULL;
-    while (data != -1)
-    {
-        Node *newNode = new Node(data);
-        if (head == NULL)
-        {
-            head = newNode;
-            tail = newNode;
-        }
-        else
-        {
-            tail->next = newNode;
-            tail = newNode;
-        }
-        cin >> data;
-    }
-    return head;
-}
-
-void print(Node *head)
-{
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-    cout << endl;
-}
+#include "FINALLinkedList.hpp"
 Node *deleteNode(Node *head, int i)
 {
     Node *temp = head;
@@ -79,7 +32,7 @@ Node *deleteNode(Node *head, int i)
 
 int main()
 {
-    Node *head = takeinput();
+    Node *head = takeInput();
     int pos;
     cin >> pos;
     head = deleteNode(head, pos);
