7acbd72461132e66d63d49e866a7440af9a18992
Kaustav Ghosh <teetangh@gmail.com>
2020-03-02 21:00:12 +0530

    Linked List 2 lot of Work

---
+++ CodeZen/Data Structures/Linked List/Linked list 2/prog01midPointOfLL.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "CNLinkedList.hpp"
+
+node *midpoint_linkedlist(node *head)
+{
+    node *slow = head;
+    node *fast = head->next;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+int main()
+{
+    node *head = takeinput();
+    node *mid = midpoint_linkedlist(head);
+    cout << mid->data;
+    return 0;
+}
---
+++ CodeZen/Data Structures/Linked List/Linked list 2/prog03mergeSortUsingLL.cpp
@@ -0,0 +1,85 @@
+//merge sort recursive
+#include <iostream>
+// #include "CNLinkedList.hpp"
+#include "FINALLinkedList.hpp"
+
+Node *mergeTwoLLs(Node *head1, Node *head2)
+{
+    Node *temp1 = head1;
+    Node *temp2 = head2;
+    Node *temp3 = NULL;
+    Node *temp3head;
+    while (temp1 != NULL && temp2 != NULL)
+    {
+        if (temp1->data < temp2->data)
+        {
+            Node *newNode = new Node(temp1->data);
+            if (temp3 == NULL)
+            {
+                temp3 = newNode;
+                temp3head = temp3;
+            }
+            else
+            {
+                temp3->next = newNode;
+                temp3 = temp3->next;
+            }
+            temp1 = temp1->next;
+        }
+        else
+        {
+            Node *newNode = new Node(temp2->data);
+            if (temp3 == NULL)
+            {
+                temp3 = newNode;
+                temp3head = temp3;
+            }
+            else
+            {
+                temp3->next = newNode;
+                temp3 = temp3->next;
+            }
+            temp2 = temp2->next;
+        }
+    }
+    while (temp1 != NULL)
+    {
+        Node *newNode = new Node(temp1->data);
+        if (temp3 == NULL)
+        {
+            temp3 = newNode;
+            temp3head = temp3;
+        }
+        else
+        {
+            temp3->next = newNode;
+            temp3 = temp3->next;
+        }
+
+        temp1 = temp1->next;
+    }
+    while (temp2 != NULL)
+    {
+        Node *newNode = new Node(temp2->data);
+        if (temp3 == NULL)
+        {
+            temp3 = newNode;
+            temp3head = temp3;
+        }
+        else
+        {
+            temp3->next = newNode;
+            temp3 = temp3->next;
+        }
+        temp2 = temp2->next;
+    }
+    return temp3head;
+}
+int main()
+{
+    Node *head1 = takeInput_Better();
+    Node *head2 = takeInput_Better();
+    Node *head3 = mergeTwoLLs(head1, head2);
+    print(head3);
+    return 0;
+}
