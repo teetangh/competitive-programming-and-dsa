7960b6d7e77c67c459bae7c4d4d6505d1b54ca82
Kaustav Ghosh <teetangh@gmail.com>
2020-03-02 19:14:27 +0530

    linkedList2.cpp added

---
+++ CodeZen/Data Structures/Linked List/Lectures/LinkedList2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+void print(Node *head)
+{
+    while (head->next != NULL)
+    {
+        cout << head->data;
+        head = head->next;
+    }
+}
+
+int main()
+{
+    // Statically
+    Node n1(1);
+    Node *head = &n1;
+
+    Node n2(2);
+    Node n3(3);
+    Node n4(4);
+    Node n5(5);
+
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = &n5;
+
+    // Printing the Linked List
+    print(head);
+}
