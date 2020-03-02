6a010f0c13e9de71095f3e6353c18b363e40f141
Kaustav Ghosh <teetangh@gmail.com>
2020-03-02 21:02:04 +0530

    trying reverse linked list recursive

---
+++ CodeZen/Data Structures/Linked List/Linked list 2/prog04reverseLinkedListRecursive.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "CNLinkedList.hpp"
+
+node *reverse_linked_list_rec(node *head)
+{
+    //write your recursive code here
+    
+}
+
+int main()
+{
+    node *head = takeinput();
+    head = reverse_linked_list_rec(head);
+    print(head);
+    return 0;
+}
