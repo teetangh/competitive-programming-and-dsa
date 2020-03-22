a931ab602300929a54ccda4700c04be98702df47
Kaustav Ghosh <teetangh@gmail.com>
2020-03-22 10:58:07 +0530

    deleted whole bunch of files

--- CodeZen/Data Structures/05 Time and Space Complexity/prog04pairSumArray.cpp
+++ CodeZen/Data Structures/05 Time and Space Complexity/prog04pairSumArray.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 using namespace std;
 
+void merge(int input[], int lb, int ub, int size)
+{
+    int i = lb;
+    int k = 0;
+    int mid = (lb + ub) / 2;
+    int j = mid + 1;
+
+    int *arr = new int[size];
+
+    while (i <= mid && j <= ub)
+    {
+        if (input[i] <= input[j])
+            arr[k++] = input[i++];
+        else
+            arr[k++] = input[j++];
+    }
+    while (i <= mid)
+        arr[k++] = input[i++];
+    while (j <= ub)
+        arr[k++] = input[j++];
+
+    for (k = 0; k < size; k++)
+        input[k] = arr[k];
+}
+void mergeSort(int input[], int size)
+{
+    int lb = 0;
+    int ub = size - 1;
+    int mid;
+    if (size == 0)
+    {
+        return;
+    }
+    else if (lb < ub)
+    {
+        mid = (lb + ub) / 2;
+        mergeSort(input, mid - lb + 1);
+        mergeSort(input + mid + 1, ub - mid);
+        merge(input, lb, ub, size);
+    }
+    else
+    {
+        return;
+    }
+}
+
 void pairSum(int input[], int size, int x)
 {
     /* Don't write main().
