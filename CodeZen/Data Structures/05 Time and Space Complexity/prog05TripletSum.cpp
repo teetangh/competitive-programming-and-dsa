42f4e6e5047332d3c5b315379cea917cc3e0815b
Kaustav Ghosh <teetangh@gmail.com>
2020-03-22 12:03:44 +0530

    triplet Sum not working

---
+++ CodeZen/Data Structures/05 Time and Space Complexity/prog05TripletSum.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+void merge(int input[], int lb, int ub, int size)
+{
+    int i = lb;
+    int k = 0;
+    int mid = (lb + ub) / 2;
+    int j = mid + 1;
+
+    int *arr = new int[size];
+
+    while (i <= mid && j <= ub)
+    {
+        if (input[i] <= input[j])
+            arr[k++] = input[i++];
+        else
+            arr[k++] = input[j++];
+    }
+    while (i <= mid)
+        arr[k++] = input[i++];
+    while (j <= ub)
+        arr[k++] = input[j++];
+
+    for (k = 0; k < size; k++)
+        input[k] = arr[k];
+}
+void mergeSort(int input[], int size)
+{
+    int lb = 0;
+    int ub = size - 1;
+    int mid;
+    if (size == 0)
+    {
+        return;
+    }
+    else if (lb < ub)
+    {
+        mid = (lb + ub) / 2;
+        mergeSort(input, mid - lb + 1);
+        mergeSort(input + mid + 1, ub - mid);
+        merge(input, lb, ub, size);
+    }
+    else
+    {
+        return;
+    }
+}
+// arr - input array
+// size - size of array
+// x - sum of triplets
+
+void FindTriplet(int arr[], int size, int x)
+{
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Print output and don't return it.
+     * Taking input is handled automatically.
+     */
+    mergeSort(arr, size);
+    for (int i = 0; i < size - 2; i++)
+    {
+        for (int j = i + 1; j < size - 1; j++)
+        {
+            for (int k = j + 1; i < size; k++)
+            {
+                if (arr[i] + arr[j] + arr[k] == x)
+                {
+                    cout << arr[i] << " " << arr[j] << " " << arr[k] << endl;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int size;
+
+    int x;
+    cin >> size;
+
+    int *input = new int[1 + size];
+
+    for (int i = 0; i < size; i++)
+        cin >> input[i];
+    cin >> x;
+
+    FindTriplet(input, size, x);
+
+    return 0;
+}
