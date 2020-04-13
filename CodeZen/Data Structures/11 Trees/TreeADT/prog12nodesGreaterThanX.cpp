1d155245b638d46499c8231c87fc675cccc3cec9
Kaustav Ghosh <teetangh@gmail.com>
2020-04-13 17:21:27 +0530

    added maxSumNodes but still minor errors to debug

---
+++ CodeZen/Data Structures/11 Trees/TreeADT/prog12nodesGreaterThanX.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include "..\include\CNTree.hpp"
+using namespace std;
+
+int nodesGreaterThanX(TreeNode<int> *root, int x)
+{
+    int ans;
+
+    if (root->data > x)
+        ans = 1;
+    else
+        ans = 0;
+
+    for (int i = 0; i < root->children.size(); i++)
+        ans += nodesGreaterThanX(root->children[i], x);
+
+    return ans;
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    TreeNode<int> *root = takeInputLevelWise();
+    cout << nodesGreaterThanX(root, x) << endl;
+}
---
+++ CodeZen/Data Structures/11 Trees/TreeADT/prog13maxSumNode.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include "..\include\CNTree.hpp"
+using namespace std;
+
+TreeNode<int> *maxSumNode(TreeNode<int> *root)
+{
+    TreeNode<int> *ans = root;
+    int sum = root->data;
+
+    for (int i = 0; i < root->children.size(); i++)
+        sum += root->children[i]->data;
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        TreeNode<int> *x = root->children[i];
+        int xSum = x->data;
+
+        for (int j = 0; j < x->children.size(); j++)
+            xSum += x->children[j]->data;
+
+        if (sum > xSum)
+        {
+            ans = x;
+            sum = xSum;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    TreeNode<int> *root = takeInputLevelWise();
+    TreeNode<int> *ans = maxSumNode(root);
+    if (ans != NULL)
+    {
+        cout << ans->data << endl;
+    }
+}
