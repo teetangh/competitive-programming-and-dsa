9b71b08ac6acdccda461997f01c6768450acee4e
Kaustav Ghosh <teetangh@gmail.com>
2020-04-13 19:50:05 +0530

    Working maxSumNode

--- CodeZen/Data Structures/11 Trees/TreeADT/prog13maxSumNode.cpp
+++ CodeZen/Data Structures/11 Trees/TreeADT/prog13maxSumNode.cpp
@@ -13,13 +13,13 @@ TreeNode<int> *maxSumNode(TreeNode<int> *root)
 
     for (int i = 0; i < root->children.size(); i++)
     {
-        TreeNode<int> *x = root->children[i];
+        TreeNode<int> *x = maxSumNode(root->children[i]);
         int xSum = x->data;
 
         for (int j = 0; j < x->children.size(); j++)
             xSum += x->children[j]->data;
 
-        if (sum > xSum)
+        if (xSum > sum)
         {
             ans = x;
             sum = xSum;
@@ -27,6 +27,30 @@ TreeNode<int> *maxSumNode(TreeNode<int> *root)
     }
     return ans;
 }
+// TreeNode<int> *maxSumNode(TreeNode<int> *root)
+// {
+//     TreeNode<int> *ans = root;
+//     int sum = root->data;
+
+//     for (int i = 0; i < root->children.size(); i++)
+//         sum += root->children[i]->data;
+
+//     for (int i = 0; i < root->children.size(); i++)
+//     {
+//         TreeNode<int> *x = root->children[i];
+//         int xSum = x->data;
+
+//         for (int j = 0; j < x->children.size(); j++)
+//             xSum += x->children[j]->data;
+
+//         if (sum > xSum)
+//         {
+//             ans = x;
+//             sum = xSum;
+//         }
+//     }
+//     return ans;
+// }
 
 int main()
 {
