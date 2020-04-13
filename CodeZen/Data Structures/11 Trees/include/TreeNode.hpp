db411cc9858e9a131c9284299342026c399460d1
Kaustav Ghosh <teetangh@gmail.com>
2020-04-13 08:45:54 +0530

    added TreeUse.cpp

---
+++ CodeZen/Data Structures/11 Trees/include/TreeNode.hpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode
+{
+public:
+    T data;
+    vector<TreeNode<> *> children;
+    // T is implicit here
+    // vector<TreeNode<T> *> children;
+
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
