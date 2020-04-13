6dbf6c7592250f2acee100b9a40bf3331205480a
Kaustav Ghosh <teetangh@gmail.com>
2020-04-13 10:57:13 +0530

    major renaming of files in Trees only

---
+++ CodeZen/Data Structures/11 Trees/TreeADT/prog06TreeHeightRecursive.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include "..\include\CNTree.hpp"
+using namespace std;
+
+int height(TreeNode<int> *root)
+{
+    int currMaxHeight = 0;
+
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        int tempHeight = height(root->children[i]);
+        if (tempHeight > currMaxHeight)
+            currMaxHeight = tempHeight;
+        else
+            continue;
+    }
+
+    return 1 + currMaxHeight;
+}
+int main()
+{
+    TreeNode<int> *root = takeInputLevelWise();
+    cout << height(root) << endl;
+}
