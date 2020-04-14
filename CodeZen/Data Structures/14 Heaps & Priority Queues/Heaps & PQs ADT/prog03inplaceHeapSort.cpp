5142808141f49b7c83d02b1b52be1a2595afbd15
Kaustav Ghosh <teetangh@gmail.com>
2020-04-15 01:15:50 +0530

    started doing Hash Maps

---
+++ CodeZen/Data Structures/14 Heaps & Priority Queues/Heaps & PQs ADT/prog03inplaceHeapSort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/PriorityQueue.hpp"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
---
+++ CodeZen/Data Structures/16 Tries and Huffman Coding/mapUse.cpp
@@ -0,0 +1,39 @@
+// Map -> BST -> log(n)
+// Unordered_map -> Hashtable -> O(1)
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    unordered_map<string, int> ourMap;
+
+    // insert
+    pair<string, int> p("abc", 1);
+    ourMap.insert(p);
+    ourMap["def"] = 1; //for updating keys
+
+    // Find or Access elements
+    cout << ourMap["abc"] << endl;
+    cout << ourMap.at("abc") << endl;
+
+    // cout << ourMap.at("ghi") << endl; //Throws exception when key missing
+    cout << "Size :" << ourMap.size() << endl;
+    cout << ourMap["ghi"] << endl; // Default value = 0 for non-existent items
+    cout << "Size :" << ourMap.size() << endl;
+
+    // Check Frequency
+    if (ourMap.count("ghi") > 0)
+        cout << "ghi is present" << endl;
+
+    // Erase an element
+    ourMap.erase("ghi");
+    if (ourMap.count("ghi") > 0)
+        cout << "ghi is present" << endl;
+    else
+        cout << "ghi is NOT present" << endl;
+
+    cout << "Size :" << ourMap.size() << endl;
+    return 0;
+}
