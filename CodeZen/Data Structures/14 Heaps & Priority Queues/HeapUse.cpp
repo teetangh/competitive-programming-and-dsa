4641ba8ab0cfdfc8c357e9e68bf8b95410847cce
Kaustav Ghosh <teetangh@gmail.com>
2020-04-14 05:46:50 +0530

    PriorityQueue.hpp , Heapuse.cpp and Heapsort OKAY

---
+++ CodeZen/Data Structures/14 Heaps & Priority Queues/HeapUse.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "include/PriorityQueue.hpp"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    PriorityQueue p;
+    p.insert(100);
+    p.insert(10);
+    p.insert(15);
+    p.insert(4);
+    p.insert(17);
+    p.insert(21);
+    p.insert(67);
+
+    cout << p.getSize() << endl;
+    cout << p.getMin() << endl;
+
+    // Actually HeapSort
+    while (!p.isEmpty())
+    {
+        cout << p.CNremoveMin() << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
--- CodeZen/Data Structures/14 Heaps & Priority Queues/include/PriorityQueue.hpp
+++ CodeZen/Data Structures/14 Heaps & Priority Queues/include/PriorityQueue.hpp
@@ -8,6 +8,10 @@ private:
     vector<int> pq;
 
 public:
+    PriorityQueue()
+    {
+    }
+
     // Checks if the Priority Queue is empty
     bool isEmpty()
     {
@@ -58,8 +62,10 @@ public:
         // parentIndex = (childIndex - 1) / 2;
     }
 
-    int deleteMin()
+    //Delete from Heap and DOWN-HEAPIFY (or RemoveMin)
+    int CNremoveMin()
     {
+        // In case PQ is empty
         if (isEmpty())
             return 0;
 
@@ -68,51 +74,79 @@ public:
         pq.pop_back();
 
         // Down-Heapify
-    }
-
-    //Delete from Heap and DOWN-HEAPIFY (or RemoveMin)
-    int removeMin()
-    {
-        int ans = pq[0];
         int parentIndex = 0;
-        swap(pq[parentIndex], pq[pq.size() - 1]);
-
         int leftChildIndex = 2 * parentIndex + 1;
         int rightChildIndex = 2 * parentIndex + 2;
 
-        int minIndex = parentIndex;
-        while (leftChildIndex && rightChildIndex < pq.size())
+        while (leftChildIndex < pq.size())
         {
-            int minValue = min(pq[parentIndex], pq[leftChildIndex], pq[rightChildIndex]);
+            int minIndex = parentIndex;
 
-            if (minValue == pq[parentIndex])
-                return ans;
-            else if (minValue == pq[leftChildIndex])
+            if (pq[minIndex] > pq[leftChildIndex])
                 minIndex = leftChildIndex;
-            else
+            if (rightChildIndex < pq.size() && pq[minIndex] > pq[rightChildIndex])
                 minIndex = rightChildIndex;
 
-            swap(pq[parentIndex], pq[minIndex]);
+            // In case the above the above 2 if's failed
+            // then the parent is at its correct position
+            if (minIndex == parentIndex)
+                break;
+
+            int temp = pq[minIndex];
+            pq[minIndex] = pq[parentIndex];
+            pq[parentIndex] = temp;
 
             parentIndex = minIndex;
             leftChildIndex = 2 * parentIndex + 1;
             rightChildIndex = 2 * parentIndex + 2;
         }
 
-        if (leftChildIndex < pq.size() && rightChildIndex >= pq.size())
-        {
-            int minValue = min(pq[parentIndex], pq[leftChildIndex]);
-            if (minValue == pq[parentIndex])
-                return ans;
-            else if (minValue == pq[leftChildIndex])
-                swap(pq[parentIndex], pq[leftChildIndex]);
-        }
-
         return ans;
     }
 
-    PriorityQueue()
-    {
-    }
-    ~PriorityQueue();
+    //Delete from Heap and DOWN-HEAPIFY (or RemoveMin)
+    // int MYremoveMin()
+    // {
+    //     int ans = pq[0];
+    //     int parentIndex = 0;
+    //     swap(pq[parentIndex], pq[pq.size() - 1]);
+
+    //     int leftChildIndex = 2 * parentIndex + 1;
+    //     int rightChildIndex = 2 * parentIndex + 2;
+
+    //     int minIndex = parentIndex;
+    //     while (leftChildIndex && rightChildIndex < pq.size())
+    //     {
+    //         int minValue = min(pq[parentIndex], pq[leftChildIndex], pq[rightChildIndex]);
+
+    //         if (minValue == pq[parentIndex])
+    //             return ans;
+    //         else if (minValue == pq[leftChildIndex])
+    //             minIndex = leftChildIndex;
+    //         else
+    //             minIndex = rightChildIndex;
+
+    //         swap(pq[parentIndex], pq[minIndex]);
+
+    //         parentIndex = minIndex;
+    //         leftChildIndex = 2 * parentIndex + 1;
+    //         rightChildIndex = 2 * parentIndex + 2;
+    //     }
+
+    //     if (leftChildIndex < pq.size() && rightChildIndex >= pq.size())
+    //     {
+    //         int minValue = min(pq[parentIndex], pq[leftChildIndex]);
+    //         if (minValue == pq[parentIndex])
+    //             return ans;
+    //         else if (minValue == pq[leftChildIndex])
+    //             swap(pq[parentIndex], pq[leftChildIndex]);
+    //     }
+
+    //     return ans;
+    // }
+
+    // PriorityQueue()
+    // {
+    // }
+    // ~PriorityQueue();
 };
