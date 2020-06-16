8ec0c09d3e43f63a30a286e792bc2952eddacb6e
Kaustav Ghosh <teetangh@gmail.com>
2020-06-16 21:37:01 +0530

    renamed files and added comments

--- CodeZen/Data Structures/02 Dynamic Allocation/prog03dynamic_allocation.cpp
+++ CodeZen/Data Structures/02 Dynamic Allocation/prog03dynamic_allocation.cpp
@@ -3,15 +3,21 @@ using namespace std;
 
 int main()
 {
+    // Creating stuff in heap memory
     int *p = new int;
     *p = 10;
     cout << *p << endl;
 
+    // More stuff
     double *pd = new double;
     char *c = new char;
 
+    // Integer array of defined size in heap
+    // 200 bytes in the heap
+    // 8 bytes in the stack
     int *pa = new int[50];
 
+    // Declaring a user-defined size dynamically allocated array in heap
     int n;
     cin >> n;
 
@@ -19,11 +25,23 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> pa2[i];
 
+    // Calculating the maximum in the array
     int max = INT8_MIN;
     for (int i = 0; i < n; i++)
     {
         if (max < pa2[i])
             max = pa2[i];
     }
+    // Printing the maximum element
     cout << "MAX IS " << max << endl;
+
+    // Syntax for deleting dynamically allocated variables and arrays
+    int *temp = new int;
+    delete temp;
+
+    temp = new int;
+    delete temp;
+
+    temp = new int[100];
+    delete[] p;
 }
