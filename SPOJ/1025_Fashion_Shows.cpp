ed23cf3d849a6ff3aedd938de80c84257d1073f3
Kaustav Ghosh <teetangh@gmail.com>
2020-08-03 19:34:42 +0530

    solved the fashion show problem

---
+++ SPOJ/1025_Fashion_Shows.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printVOV(vector<vector<int>> my_vec)
+{
+    for (auto &vec : my_vec)
+    {
+        for (auto &ele : vec)
+        {
+            cout << ele << " ";
+        }
+        cout << endl;
+    }
+}
+
+int elementwise_sum_of_2_vectors(vector<int> v1, vector<int> v2)
+{
+    vector<int> v3(v1.size());
+    std::transform(v1.begin(), v1.end(), v2.begin(), v3.begin(), std::multiplies<int>());
+
+    int element_wise_sum = accumulate(v3.begin(), v3.end(), 0);
+
+    return element_wise_sum;
+}
+
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    freopen("input1.txt", "r", stdin);
+    freopen("output1.txt", "w", stdout);
+#endif
+
+    int test_cases;
+    cin >> test_cases;
+    int test_cases2 = test_cases;
+
+    vector<int> vec;
+    vector<vector<int>> my_vec;
+
+    while (test_cases--)
+    {
+        int num_of_participants;
+        cin >> num_of_participants;
+
+        vec.clear();
+        int sub_test_cases = 2;
+        while (sub_test_cases--)
+        {
+            int x;
+            vec.clear();
+            for (int i = 0; i < num_of_participants; i++)
+            {
+                cin >> x;
+                vec.push_back(x);
+            }
+            sort(vec.begin(), vec.end());
+            my_vec.push_back(vec);
+        }
+    }
+    // printVOV(my_vec);
+
+    queue<int> mq;
+
+    for (int i = 0; i < test_cases2; i++)
+    {
+        // int current_sum = 0;
+        mq.push(elementwise_sum_of_2_vectors(my_vec[2 * i], my_vec[(2 * i) + 1]));
+    }
+
+    while (!mq.empty())
+    {
+        int temp = mq.front();
+        mq.pop();
+        cout << temp << endl;
+    }
+
+    return 0;
+}
