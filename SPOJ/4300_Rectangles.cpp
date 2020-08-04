e4022434aadc18e5b3eda6cfb6bc2bccbaf21cdc
Kaustav Ghosh <teetangh@gmail.com>
2020-08-04 11:42:18 +0530

    solved the number of rectangles problem

---
+++ SPOJ/4300_Rectangles.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int num_of_rectangles(int squares)
+{
+    // just count the no of divisors upto((floor)sqrt(i)) where 1<=i<=n
+    int rectangles = 0;
+    for (int i = 1; i <= squares; i++)
+    {
+        for (int j = 1; j * j <= i; j++)
+        {
+            if (i % j == 0)
+                rectangles++;
+        }
+    }
+    return rectangles;
+}
+
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    freopen("input1.txt", "r", stdin);
+    freopen("output1.txt", "w", stdout);
+#endif
+    int squares;
+    cin >> squares;
+    cout << num_of_rectangles(squares);
+    return 0;
+}
