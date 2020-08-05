2f40be95c859db517e4973575d063665643aab39
Kaustav Ghosh <teetangh@gmail.com>
2020-08-05 12:41:37 +0530

    started cows

---
+++ SPOJ/0297_Aggressive_cows.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUGE
+    freopen("input1.txt", "r", stdin);
+    freopen("output1.txt", "w", stdout);
+#endif
+
+    int test_cases;
+    cin >> test_cases;
+
+    while (test_cases--)
+    {
+    }
+
+    return 0;
+}
