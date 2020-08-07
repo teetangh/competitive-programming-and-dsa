09b1964b14b77e29b5f406c8fe2644ad0e8c500b
Kaustav Ghosh <teetangh@gmail.com>
2020-08-07 13:17:55 +0530

    decided C,C++,STL syle and completed Subset Sum

---
+++ Youtube/Basic ALGO/DP/DP Aditya Verma/prog007_SubSet_Sum_Problem_bottom_up.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <iomanip>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print_MOM(bool **memoization_matrix, int array_size, int required_sum)
+{
+    cout << "Current Memoization Matrix Status" << endl;
+    for (int i = 0; i < array_size + 1; i++)
+    {
+        for (int j = 0; j < required_sum + 1; j++)
+            cout << setw(2) << memoization_matrix[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+
+bool subset_sum_problem(vector<int> elements_array, int array_size, int required_sum)
+{
+    bool **memoization_matrix = new bool *[array_size + 1];
+    for (int i = 0; i < array_size + 1; i++)
+        memoization_matrix[i] = new bool[required_sum + 1];
+
+    for (int i = 0; i < array_size + 1; i++)
+    {
+        for (int j = 0; j < required_sum + 1; j++)
+        {
+            if (i == 0)
+                memoization_matrix[i][j] = false;
+            if (j == 0)
+                memoization_matrix[i][j] = true;
+        }
+    }
+
+    // print_MOM(memoization_matrix, array_size, required_sum);
+
+    for (int i = 1; i < array_size + 1; i++)
+    {
+        for (int j = 1; j < required_sum + 1; j++)
+        {
+            // print_MOM(memoization_matrix, array_size, required_sum);
+            if (elements_array[i - 1] <= required_sum)
+                memoization_matrix[i][j] = (memoization_matrix[i - 1][j - elements_array[i - 1]]) ||
+                                           (memoization_matrix[i - 1][j]);
+            else
+                memoization_matrix[i][j] = (memoization_matrix[i - 1][j]);
+        }
+    }
+
+    print_MOM(memoization_matrix, array_size, required_sum);
+    bool answer = memoization_matrix[array_size][required_sum];
+
+    for (int i = 0; i < array_size + 1; i++)
+        delete[] memoization_matrix[i];
+    delete[] memoization_matrix;
+
+    return answer;
+}
+
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    freopen("input1.txt", "r", stdin);
+    freopen("output1.txt", "w", stdout);
+#endif
+
+    int array_size, required_sum;
+    cin >> array_size >> required_sum;
+
+    vector<int> elements_array(array_size);
+
+    for (int i = 0; i < array_size; i++)
+        cin >> elements_array[i];
+
+    bool answer = subset_sum_problem(elements_array, array_size, required_sum);
+    cout << answer;
+
+    return 0;
+}
