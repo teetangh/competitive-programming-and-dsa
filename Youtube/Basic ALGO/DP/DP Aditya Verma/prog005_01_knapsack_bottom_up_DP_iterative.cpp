39e4a24908239e7aa5a32b051281c538b2a66d7e
Kaustav Ghosh <teetangh@gmail.com>
2020-08-07 11:20:26 +0530

    knapsack bottom up DP but getting seg fault

--- Youtube/Basic ALGO/DP/DP Aditya Verma/prog005_01_knapsack_bottom_up_DP_iterative.cpp
+++ Youtube/Basic ALGO/DP/DP Aditya Verma/prog005_01_knapsack_bottom_up_DP_iterative.cpp
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 using namespace std;
 
-int knapsack(int weight_array[], int value_array[], int bag_capacity, int num_of_items)
+int knapsack_bottom_up(int weight_array[], int value_array[], int bag_capacity, int num_of_items)
 {
 
     int **memoization_matrix = new int *[num_of_items + 1];
     for (int i = 0; i < num_of_items + 1; ++i)
         memoization_matrix[i] = new int[bag_capacity + 1];
 
+    // Can't do in O(n) coz matrix not square
     for (int i = 0; i < num_of_items + 1; i++)
     {
         for (int j = 0; j < bag_capacity + 1; j++)
@@ -18,9 +19,25 @@ int knapsack(int weight_array[], int value_array[], int bag_capacity, int num_of
         }
     }
 
+    for (int i = 1; i < num_of_items + 1; i++)
+    {
+        for (int j = 1; i < bag_capacity + 1; i++)
+        {
+            if (weight_array[i + 1] <= j)
+                memoization_matrix[i][j] = max(value_array[i - 1] + memoization_matrix[i - 1][j - weight_array[i - 1]], memoization_matrix[i - 1][j]);
+
+            else
+                memoization_matrix[i][j] = memoization_matrix[i - 1][j];
+        }
+    }
+
+    int answer = memoization_matrix[num_of_items][bag_capacity];
+
     for (int i = 0; i < num_of_items + 1; i++)
         delete[] memoization_matrix[i];
     delete[] memoization_matrix;
+
+    return answer;
 }
 
 int main(int argc, char const *argv[])
@@ -45,7 +62,7 @@ int main(int argc, char const *argv[])
         value_array[i] = curr_item_value;
     }
 
-    int answer = knapsack(weight_array, value_array, bag_capacity, num_of_items);
+    int answer = knapsack_bottom_up(weight_array, value_array, bag_capacity, num_of_items);
     cout << answer;
 
     return 0;
