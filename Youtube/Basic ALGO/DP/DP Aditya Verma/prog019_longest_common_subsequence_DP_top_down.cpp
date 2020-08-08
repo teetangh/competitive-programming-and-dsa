bbb71c479156546b3b5a25ea82989d07749a98ec
Kaustav Ghosh <teetangh@gmail.com>
2020-08-08 20:37:26 +0530

    completed LCS DP top down

---
+++ Youtube/Basic ALGO/DP/DP Aditya Verma/prog019_longest_common_subsequence_DP_top_down.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int longest_common_subsequence(string X, string Y, int m, int n)
+{
+    if (m == 0 || n == 0)
+        return 0;
+
+    else if (X[m - 1] == Y[n - 1])
+        return 1 + longest_common_subsequence(X, Y, m - 1, n - 1);
+    else
+        return max(longest_common_subsequence(X, Y, m, n - 1), longest_common_subsequence(X, Y, m - 1, n));
+}
+
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    freopen("xinput1.txt", "r", stdin);
+    freopen("xoutput1.txt", "w", stdout);
+#endif
+
+    string X, Y;
+    cin >> X >> Y;
+    int m = X.length();
+    int n = Y.length();
+
+    int answer = longest_common_subsequence(X, Y, m, n);
+    cout << answer;
+
+    return 0;
+}
+
+// // Sample IO
+// batman
+
+// aman
+
+// 4
