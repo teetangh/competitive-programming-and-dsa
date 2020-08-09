380410f886fb9e5d4e819decd527c1105f211d2a
Kaustav Ghosh <teetangh@gmail.com>
2020-08-09 17:42:42 +0530

    updated longest common subtring

---
+++ Youtube/Basic ALGO/DP/DP Aditya Verma/prog022_longest_common_substring_DP_bottom_up.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int longest_common_subsequence(string X, string Y, int m, int n)
+{
+    int default_value = 0;
+    vector<vector<int>> tabulation_matrix;
+    tabulation_matrix.resize(X.length() + 1, vector<int>(Y.length() + 1, default_value));
+
+    for (int i = 1; i < X.length() + 1; i++)
+    {
+        for (int j = 1; j < Y.length() + 1; j++)
+        {
+            if (X[i - 1] == Y[j - 1])
+                tabulation_matrix[i][j] = 1 + tabulation_matrix[i - 1][j - 1];
+            else
+                tabulation_matrix[i][j] = 0;
+        }
+    }
+
+    return tabulation_matrix[X.length()][Y.length()];
+}
+
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    freopen("xinput1.txt", "r", stdin);
+    freopen("xoutput1.txt", "w", stdout);
+#endif
+    string X, Y;
+    cin >> X >> Y;
+    int answer = longest_common_subsequence(X, Y, X.length(), Y.length());
+    cout << answer;
+    return 0;
+}
+
+// // Sample IO
+// batman
+
+// aman
+
+// 3
