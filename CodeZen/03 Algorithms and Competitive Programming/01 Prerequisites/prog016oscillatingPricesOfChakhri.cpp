164b2f5c8b0d9409c0c4125b3a9802a77d522a35
Kaustav Ghosh <teetangh@gmail.com>
2020-08-11 11:59:59 +0530

    accepted prog016oscillatingPricesOfChakhri.cpp

--- CodeZen/03 Algorithms and Competitive Programming/01 Prerequisites/prog016oscillatingPricesOfChakhri.cpp
+++ CodeZen/03 Algorithms and Competitive Programming/01 Prerequisites/prog016oscillatingPricesOfChakhri.cpp
@@ -1,42 +1,41 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
+#include <climits>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
 #ifndef ONLINE_JUDGE
     freopen("xinput.txt", "r", stdin);
     freopen("xoutput.txt", "w", stdout);
 #endif
-
-    // Write your code here
     int array_size;
     cin >> array_size;
     vector<int> arr(array_size);
 
-    int n;
+    for (int i = 0; i < array_size; i++)
+        cin >> arr[i];
+
+    int current_max = arr[0];
+    int current_min = arr[array_size - 1];
+
+    int current_difference = INT_MIN;
+
     for (int i = 0; i < array_size; i++)
     {
-        cin >> n;
-        arr[i] = n;
+        for (int j = array_size - 1; j > i; j--)
+        {
+            if (arr[j] - arr[i] > current_difference)
+                current_difference = arr[j] - arr[i];
+        }
     }
 
-    int min = arr[0];
-    int max = arr[0];
-
-    for (int i = 0; i < array_size; i++)
-    {
-        if (arr[i] < min)
-            min = arr[i];
-        if (arr[i] > max)
-            max = arr[i];
-    }
-
-    cout << max << " " << min << endl;
-    cout << (max - min);
-
+    cout << current_difference;
     return 0;
 }
+// // Sample IO 
+// 7
+// 62 63 70 66 64 68 61
+
+// 8
