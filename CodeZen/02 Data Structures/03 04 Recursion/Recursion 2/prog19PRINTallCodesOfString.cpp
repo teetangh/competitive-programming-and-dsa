2902cb53059420c98f47ce71d7c240944d2b6830
Kaustav Ghosh <teetangh@gmail.com>
2020-08-16 13:00:11 +0530

    update print all codes of string recursion program

--- CodeZen/02 Data Structures/03 04 Recursion/Recursion 2/prog19PRINTallCodesOfString.cpp
+++ CodeZen/02 Data Structures/03 04 Recursion/Recursion 2/prog19PRINTallCodesOfString.cpp
@@ -2,7 +2,8 @@
 // #include "solution.h"
 // using namespace std;
 
-#include <string.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
 void printHelperFunction(string input, string output)
@@ -18,19 +19,18 @@ void printHelperFunction(string input, string output)
     leftRecursion = 'a' + (input[0] - 48) - 1;
 
     bool flag = false;
-    if ((input[1]) != '\0')
+    if (input[1] != '\0')
     {
         rightRecursion = 'a' + (10 * (input[0] - 48) + (input[1] - 48)) - 1;
-        if (rightRecursion >= 10 && rightRecursion <= 26)
+        if (rightRecursion >= 'a' && rightRecursion <= 'z')
             flag = true;
     }
 
     printHelperFunction(input.substr(1), output + leftRecursion);
-
+    string output2 = output + rightRecursion;
     if (flag == true)
     {
-
-        printHelperFunction(input.substr(2), output + rightRecursion);
+        printHelperFunction(input.substr(2), output2);
     }
 }
 
@@ -39,7 +39,6 @@ void printAllPossibleCodes(string input)
     string output = "";
     printHelperFunction(input, output);
 }
-
 int main()
 {
 #ifndef ONLINE_JUDGE
