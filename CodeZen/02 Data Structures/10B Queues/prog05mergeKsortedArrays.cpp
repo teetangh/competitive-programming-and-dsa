3a206e11f81aa6bf2516ee360b045b1cf215a734
Kaustav Ghosh <teetangh@gmail.com>
2020-08-16 22:48:33 +0530

    added priority queue program

---
+++ CodeZen/02 Data Structures/10B Queues/prog05mergeKsortedArrays.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+// #include "Solution.h"
+
+#include <queue>
+#include <stack>
+vector<int> mergeKSortedArrays(vector<vector<int> *> input)
+{
+    priority_queue<int> pq;
+
+    for (auto ele : input)
+    {
+        for (int i = 0; i < ele->size(); i++)
+        {
+            pq.push(ele[i]);
+        }
+    }
+
+    stack<int> s;
+    while (!pq.empty())
+    {
+        int temp = pq.top();
+        pq.pop();
+        s.push(temp);
+    }
+
+    vector<int> temp_output;
+    while (!s.empty())
+    {
+        int temp2 = s.top();
+        s.pop();
+        temp_output.push_back(temp2);
+    }
+
+    return temp_output;
+}
+
+int main()
+{
+
+    int k;
+    cin >> k;
+    vector<vector<int> *> input;
+    for (int j = 1; j <= k; j++)
+    {
+        int size;
+        cin >> size;
+        vector<int> *current = new vector<int>;
+
+        for (int i = 0; i < size; i++)
+        {
+            int a;
+            cin >> a;
+            current->push_back(a);
+        }
+        input.push_back(current);
+    }
+
+    vector<int> output = mergeKSortedArrays(input);
+
+    for (int i = 0; i < output.size(); i++)
+        cout << output[i] << " ";
+
+    return 0;
+}
