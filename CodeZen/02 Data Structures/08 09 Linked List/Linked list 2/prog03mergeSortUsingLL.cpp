6f0dfb220b40cd4425c3e4d6c6d6f6d270945d71
Kaustav Ghosh <teetangh@gmail.com>
2020-08-22 19:29:34 +0530

    updated mergesort using linked list

--- CodeZen/02 Data Structures/08 09 Linked List/Linked list 2/prog03mergeSortUsingLL.cpp
+++ CodeZen/02 Data Structures/08 09 Linked List/Linked list 2/prog03mergeSortUsingLL.cpp
@@ -1,42 +1,54 @@
 //merge sort recursive
 #include <iostream>
-// #include "CNLinkedList.hpp"
-#include "../include/FINALLinkedList.hpp"
+#include "../include/CNLinkedList.hpp"
+// #include "../include/FINALLinkedList.hpp"
 
-Node *mergeTwoLLs(Node *head1, Node *head2)
+node *midpoint_linkedlist(node *head)
 {
-    Node *temp1 = head1;
-    Node *temp2 = head2;
-    Node *temp3 = NULL;
-    Node *temp3head;
+    node *slow = head;
+    node *fast = head->next;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+node *mergeTwoLLs(node *head1, node *head2)
+{
+    node *temp1 = head1;
+    node *temp2 = head2;
+    node *temp3 = NULL;
+    node *temp3head;
     while (temp1 != NULL && temp2 != NULL)
     {
         if (temp1->data < temp2->data)
         {
-            Node *newNode = new Node(temp1->data);
+            node *newnode = new node(temp1->data);
             if (temp3 == NULL)
             {
-                temp3 = newNode;
+                temp3 = newnode;
                 temp3head = temp3;
             }
             else
             {
-                temp3->next = newNode;
+                temp3->next = newnode;
                 temp3 = temp3->next;
             }
             temp1 = temp1->next;
         }
         else
         {
-            Node *newNode = new Node(temp2->data);
+            node *newnode = new node(temp2->data);
             if (temp3 == NULL)
             {
-                temp3 = newNode;
+                temp3 = newnode;
                 temp3head = temp3;
             }
             else
             {
-                temp3->next = newNode;
+                temp3->next = newnode;
                 temp3 = temp3->next;
             }
             temp2 = temp2->next;
@@ -44,15 +56,15 @@ Node *mergeTwoLLs(Node *head1, Node *head2)
     }
     while (temp1 != NULL)
     {
-        Node *newNode = new Node(temp1->data);
+        node *newnode = new node(temp1->data);
         if (temp3 == NULL)
         {
-            temp3 = newNode;
+            temp3 = newnode;
             temp3head = temp3;
         }
         else
         {
-            temp3->next = newNode;
+            temp3->next = newnode;
             temp3 = temp3->next;
         }
 
@@ -60,26 +72,39 @@ Node *mergeTwoLLs(Node *head1, Node *head2)
     }
     while (temp2 != NULL)
     {
-        Node *newNode = new Node(temp2->data);
+        node *newnode = new node(temp2->data);
         if (temp3 == NULL)
         {
-            temp3 = newNode;
+            temp3 = newnode;
             temp3head = temp3;
         }
         else
         {
-            temp3->next = newNode;
+            temp3->next = newnode;
             temp3 = temp3->next;
         }
         temp2 = temp2->next;
     }
     return temp3head;
 }
+node *mergeSort(node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+    node *mid = midpoint_linkedlist(head);
+    node *part1 = head;
+    node *part2 = mid->next;
+    mid->next = NULL;
+    part1 = mergeSort(part1);
+    part2 = mergeSort(part2);
+    head = mergeTwoLLs(part1, part2);
+    return head;
+}
 int main()
 {
-    Node *head1 = takeInput_Better();
-    Node *head2 = takeInput_Better();
-    Node *head3 = mergeTwoLLs(head1, head2);
+    node *head1 = takeinput();
+    node *head2 = takeinput();
+    node *head3 = mergeTwoLLs(head1, head2);
     print(head3);
     return 0;
 }
