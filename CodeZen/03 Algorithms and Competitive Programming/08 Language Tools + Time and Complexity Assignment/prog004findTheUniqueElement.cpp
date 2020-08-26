f7575f2231afffc559fd6826b4f71d43925169ba
Kaustav Ghosh <teetangh@gmail.com>
2020-08-26 16:07:38 +0530

    updated the unique element

---
+++ CodeZen/03 Algorithms and Competitive Programming/08 Language Tools + Time and Complexity Assignment/prog004findTheUniqueElement.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+// #include "solution.h"
+using namespace std;
+
+// arr - input array
+// size - size of array
+#include <bits/stdc++.h>
+
+int FindUnique2(int arr[], int size)
+{
+
+    map<int, int> mp;
+
+    for (int i = 0; i < size; i++)
+        mp[arr[i]]++;
+
+    for (auto &ele : mp)
+    {
+        if (ele.second == 1)
+            return ele.first;
+    }
+
+    return -1;
+}
+// arr - input array
+// size - size of array
+#include <bits/stdc++.h>
+
+int FindUnique(int arr[], int size)
+{
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        bool flag = false;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                flag = true;
+                break;
+            }
+        }
+        if (flag == false)
+            return arr[i];
+    }
+
+    return -1;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("xinput.txt", "r", stdin);
+    freopen("xoutput.txt", "w", stdout);
+#endif
+
+    int size;
+
+    cin >> size;
+    int *input = new int[1 + size];
+
+    for (int i = 0; i < size; i++)
+        cin >> input[i];
+
+    cout << FindUnique(input, size) << endl;
+
+    return 0;
+}
