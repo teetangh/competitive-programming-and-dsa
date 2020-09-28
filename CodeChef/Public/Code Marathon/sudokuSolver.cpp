42366932f517ccca84027bc8f84b379196840e3e
Kaustav Ghosh <teetangh@gmail.com>
2020-09-28 13:23:24 +0530

    Built Sudoku Solver from Scratch without internet

--- CodeChef/Public/Code Marathon/sudokuSolver.cpp
+++ CodeChef/Public/Code Marathon/sudokuSolver.cpp
@@ -115,19 +115,19 @@ bool solveSudokuHelper(vector<vector<int>> &board)
                     else
                     {
                         flagFilled = true;
-                        break;
+                        bool answer = solveSudokuHelper(board);
+                        if (answer)
+                            return answer;
+                        else
+                        {
+                            board[i][j] = 0;
+                            flagFilled = false;
+                            continue;
+                        }
                     }
                 }
                 if (flagFilled == false)
                     return false;
-                else
-                {
-                    bool answer = solveSudokuHelper(board);
-                    if (answer)
-                        return answer;
-                    else
-                        return false;
-                }
             }
         }
     }
@@ -160,3 +160,24 @@ int main()
 
     solveSudoku(board);
 }
+
+// // Sample IO
+// 3 0 6 5 0 8 4 0 0
+// 5 2 0 0 0 0 0 0 0
+// 0 8 7 0 0 0 0 3 1
+// 0 0 3 0 1 0 0 8 0
+// 9 0 0 8 6 3 0 0 5
+// 0 5 0 0 9 0 6 0 0
+// 1 3 0 0 0 0 2 5 0
+// 0 0 0 0 0 0 0 7 4
+// 0 0 5 2 0 6 3 0 0
+
+// 3 1 6 5 7 8 4 9 2 
+// 5 2 9 1 3 4 7 6 8 
+// 4 8 7 6 2 9 5 3 1 
+// 2 6 3 4 1 5 9 8 7 
+// 9 7 4 8 6 3 1 2 5 
+// 8 5 1 7 9 2 6 4 3 
+// 1 3 8 9 4 7 2 5 6 
+// 6 9 2 3 5 1 8 7 4 
+// 7 4 5 2 8 6 3 1 9 
