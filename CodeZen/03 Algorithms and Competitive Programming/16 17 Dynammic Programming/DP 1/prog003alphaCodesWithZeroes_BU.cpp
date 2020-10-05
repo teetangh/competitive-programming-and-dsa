33f3fb2b8aa351e879d21d58d33ee1e0b1bb9e7a
Kaustav Ghosh <teetangh@gmail.com>
2020-10-05 12:34:36 +0530

    added alpha codes BU TD DP

--- CodeZen/03 Algorithms and Competitive Programming/16 17 Dynammic Programming/DP 1/prog003alphaCodesWithZeroes_BU.cpp
+++ CodeZen/03 Algorithms and Competitive Programming/16 17 Dynammic Programming/DP 1/prog003alphaCodesWithZeroes_BU.cpp
@@ -8,16 +8,23 @@ typedef long long int lli;
 
 lli num_codes3(lli *input, lli size)
 {
-    lli *output = new lli[size + 1];
+    if (size == 0 || size == 1)
+    {
+        return 1;
+    }
+
+    lli *output = (lli *)calloc(size + 1, sizeof(lli));
+
     output[0] = 1;
     output[1] = 1;
 
     for (lli i = 2; i <= size; i++)
     {
-        output[i] = output[i - 1] % MOD;
+        if (input[i - 1] != 0)
+            output[i] = output[i - 1] % MOD;
 
-        // if (input[i - 2] * 10 + input[i - 1] <= 26)
-        if ((input[i - 1] <= 6 && input[i - 2] == 2) || input[i - 2] == 1)
+        // if ((input[i - 1] <= 6 && input[i - 2] == 2) || input[i - 2] == 1)
+        if ((input[i - 2] * 10 + input[i - 1] <= 26) && input[i - 2] != 0)
             output[i] = (output[i] + output[i - 2]) % MOD;
     }
 
@@ -33,7 +40,7 @@ int main()
     {
         if (num == "0")
             break;
-        lli *input = new lli[100];
+        lli *input = new lli[5000];
         for (lli i = 0; i < num.length(); i++)
             input[i] = num[i] - '0';
 
