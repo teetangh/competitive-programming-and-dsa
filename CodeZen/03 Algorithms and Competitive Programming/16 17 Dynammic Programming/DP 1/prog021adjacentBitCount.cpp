9a77382c3f6deae23c8449ac21f20eb21543a7ad
Kaustav Ghosh <teetangh@gmail.com>
2020-10-30 22:48:04 +0530

    updated adjacent bit count

--- CodeZen/03 Algorithms and Competitive Programming/16 17 Dynammic Programming/DP 1/prog021adjacentBitCount.cpp
+++ CodeZen/03 Algorithms and Competitive Programming/16 17 Dynammic Programming/DP 1/prog021adjacentBitCount.cpp
@@ -30,9 +30,27 @@ lli adjacent_bit_count(int n, int k, int first, vector<vector<vector<int>>> &dp)
         return dp[n][k][first];
 
     if (first == 0)
-        return dp[n][k][first] = adjacent_bit_count(n - 1, k, 0, dp) + adjacent_bit_count(n - 1, k - 1, 1, dp);
+        return dp[n][k][first] = (adjacent_bit_count(n - 1, k, 0, dp) % MOD + adjacent_bit_count(n - 1, k, 1, dp) % MOD) % MOD;
     else
-        return dp[n][k][first] = adjacent_bit_count(n - 1, k, 0, dp) + adjacent_bit_count(n - 1, k, 1, dp);
+        return dp[n][k][first] = (adjacent_bit_count(n - 1, k, 0, dp) % MOD + adjacent_bit_count(n - 1, k - 1, 1, dp) % MOD) % MOD;
+}
+
+lli adjacent_bit_count2(int n, int k, int last, vector<vector<vector<int>>> &dp)
+{
+    if (k < 0)
+        return 0;
+    if (n < 0)
+    {
+        return k == 0;
+    }
+
+    int ret = dp[n][k][last];
+
+    if (ret == -1)
+        ret = (adjacent_bit_count2(n - 1, k, 0, dp) % MOD + adjacent_bit_count2(n - 1, last == 1 ? k - 1 : k, 1, dp) % MOD) % MOD;
+
+    dp[n][k][last] = ret;
+    return ret % MOD;
 }
 
 int main()
@@ -72,3 +90,14 @@ int main()
 // 8 80 73
 // 9 90 84
 // 10 100 90
+// Sample Output
+// 1 6
+// 2 63426
+// 3 1861225
+// 4 168212501
+// 5 44874764
+// 6 160916
+// 7 22937308
+// 8 99167
+// 9 15476
+// 10 23076518
