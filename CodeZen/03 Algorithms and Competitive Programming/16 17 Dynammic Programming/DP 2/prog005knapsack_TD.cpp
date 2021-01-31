c4078f7acbff70b0203789f550cc6fadabd4f8c2
Kaustav Ghosh <teetangh@gmail.com>
2021-01-31 19:57:09 +0530

    updated codezen

--- CodeZen/03 Algorithms and Competitive Programming/16 17 Dynammic Programming/DP 2/prog005knapsack_TD.cpp
+++ CodeZen/03 Algorithms and Competitive Programming/16 17 Dynammic Programming/DP 2/prog005knapsack_TD.cpp
@@ -9,11 +9,11 @@ int knapsackHelper(int *weights, int *values, int n, int maxWeight, vector<vecto
     if (n == 0 || maxWeight == 0)
         return dp[n][maxWeight] = 0;
 
-    if (dp[n][maxWeight] != -1)
+    if (dp[n][maxWeight] > 0)
         return dp[n][maxWeight];
 
-    if (weights[n] <= maxWeight)
-        return dp[n][maxWeight] = max(values[n] + knapsackHelper(weights, values, n - 1, maxWeight - weights[n], dp),
+    if (weights[n - 1] <= maxWeight)
+        return dp[n][maxWeight] = max(values[n - 1] + knapsackHelper(weights, values, n - 1, maxWeight - weights[n - 1], dp),
                                       knapsackHelper(weights, values, n - 1, maxWeight, dp));
     else
         return dp[n][maxWeight] = knapsackHelper(weights, values, n - 1, maxWeight, dp);
@@ -23,12 +23,25 @@ int knapsack(int *weights, int *values, int n, int maxWeight)
     vector<vector<int>> dp;
     dp.resize(n + 1, vector<int>(maxWeight + 1, -1));
 
-    return knapsackHelper(weights, values, n - 1, maxWeight, dp);
+    auto answer = knapsackHelper(weights, values, n, maxWeight, dp);
+    // for (auto &ele : dp)
+    // {
+    //     for (auto &elem : ele)
+    //     {
+    //         cout << elem << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    return answer;
 }
 
 int main()
 {
-
+#ifndef ONLINE_JUDGE
+    freopen("xinput.txt", "r", stdin);
+    freopen("xoutput.txt", "w", stdout);
+#endif
     int n;
     cin >> n;
     int *weights = new int[n];
