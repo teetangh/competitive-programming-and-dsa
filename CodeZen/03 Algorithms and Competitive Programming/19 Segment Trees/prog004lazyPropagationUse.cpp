be37519a6f879252b4ba75a7d256a1704c581d09
Kaustav Ghosh <teetangh@gmail.com>
2021-03-09 01:23:23 +0530

    working on lazy propagation

--- CodeZen/03 Algorithms and Competitive Programming/19 Segment Trees/prog004lazyPropagationUse.cpp
+++ CodeZen/03 Algorithms and Competitive Programming/19 Segment Trees/prog004lazyPropagationUse.cpp
@@ -2,6 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Defines
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
 void buildTree(int *arr, int *tree, int start, int end, int treeNode)
 {
 
@@ -16,43 +20,7 @@ void buildTree(int *arr, int *tree, int start, int end, int treeNode)
     buildTree(arr, tree, start, mid, 2 * treeNode);
     buildTree(arr, tree, mid + 1, end, 2 * treeNode + 1);
 
-    tree[treeNode] = tree[2 * treeNode] + tree[2 * treeNode + 1];
-}
-
-void updateTree(int *arr, int *tree, int start, int end, int treeNode, int idx, int value)
-{
-    if (start == end)
-    {
-        arr[idx] = value;
-        tree[treeNode] = value;
-        return;
-    }
-    int mid = (start + end) / 2;
-    if (idx > mid)
-        updateTree(arr, tree, mid + 1, end, 2 * treeNode + 1, idx, value);
-    else
-        updateTree(arr, tree, start, mid, 2 * treeNode, idx, value);
-
-    tree[treeNode] = tree[2 * treeNode] + tree[2 * treeNode + 1];
-}
-
-int query(int *tree, int start, int end, int treeNode, int left, int right)
-{
-
-    // Completely outside given range
-    if (start > right || end < left)
-        return 0;
-
-    // Completely inside given range
-    if (start >= left && end <= right)
-        return tree[treeNode];
-
-    // Partially inside Partially outside
-    int mid = (start + end) / 2;
-    int ans1 = query(tree, start, mid, 2 * treeNode, left, right);
-    int ans2 = query(tree, mid + 1, end, 2 * treeNode + 1, left, right);
-
-    return ans1 + ans2;
+    tree[treeNode] = min(tree[2 * treeNode], tree[2 * treeNode + 1]);
 }
 
 void updateSegmentTreeLazy(int *tree, int *lazy, int low, int high, int startRange, int endRange, int currPos, int increment)
@@ -91,7 +59,34 @@ void updateSegmentTreeLazy(int *tree, int *lazy, int low, int high, int startRan
         updateSegmentTreeLazy(tree, lazy, low, mid, startRange, endRange, 2 * currPos, increment);
         updateSegmentTreeLazy(tree, lazy, mid + 1, high, startRange, endRange, 2 * currPos + 1, increment);
 
-        // tree[currPos] = min(tree[2 * currPos], tree[2 * currPos + 1]);
-        tree[currPos] = tree[2 * currPos] + tree[2 * currPos + 1]; // Double Check Later (self derived)
+        tree[currPos] = min(tree[2 * currPos], tree[2 * currPos + 1]);
     }
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("xinput.txt", "r", stdin);   // for getting input from xinput.txt
+    freopen("xoutput.txt", "w", stdout); // for writing output to xoutput.txt
+#endif
+
+    fastio;
+
+    int arr[] = {1, 3, -2, 4};
+
+    int *tree = new int[12];
+    buildTree(arr, tree, 0, 3, 1);
+
+    int *lazy = new int[12];
+    updateSegmentTreeLazy(tree, lazy, 0, 3, 0, 3, 1, 3);
+    updateSegmentTreeLazy(tree, lazy, 0, 3, 0, 1, 1, 2);
+
+    cout << "Segment Tree" << endl;
+    for (int i = 1; i < 12; i++)
+        cout << tree[i] << " ";
+    cout << endl;
+
+    cout << "Lazy Tree" << endl;
+    for (int i = 1; i < 12; i++)
+        cout << lazy[i] << " ";
+    cout << endl;
 }
