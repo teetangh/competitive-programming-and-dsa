4b7fba252736fa6f7a9945a0e0c9d17984bf39ad
Kaustav Ghosh <teetangh@gmail.com>
2021-04-05 19:40:51 +0530

    started GFG questions + basic reverse array string

---
+++ LeetCode/Algorithms/Searching-Sorting/0033_Search_rotated_sorted_array.cpp
@@ -0,0 +1,29 @@
+// Includes
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Defines
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("xinput.txt", "r", stdin);   // for getting input from xinput.txt
+    freopen("xoutput.txt", "w", stdout); // for writing output to xoutput.txt
+#endif
+
+    fastio;
+
+    int tc;
+    cin >> tc;
+    // cout << tc;
+
+    while(tc--)
+    {
+
+    }
+}
