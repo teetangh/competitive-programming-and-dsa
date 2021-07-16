d805645ef976593b4a7afc747b63ea5f555efa19
Kaustav Ghosh <teetangh@gmail.com>
2021-07-16 16:56:20 +0530

    added more programs

--- Youtube-learning/Basic DS/Graphs/ Graphs Part 1 CodeNCode/new20feasiblePath.cpp
+++ Youtube-learning/Basic DS/Graphs/ Graphs Part 1 CodeNCode/new20feasiblePath.cpp
@@ -9,10 +9,24 @@ using namespace std;
 #define endl '\n'
 #define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 
-#define MAX 100001
+#define MAX 1000001
 vector<int> ar[MAX];
+
 int dist[MAX];
 int visited[MAX];
+int cc[MAX];
+int curr_cc;
+
+void dfs(int node)
+{
+    visited[node] = 1;
+    cc[node] = curr_cc;
+    for (auto child : ar[node])
+    {
+        if (!visited[child])
+            dfs(child);
+    }
+}
 
 int main()
 {
@@ -24,12 +38,66 @@ int main()
     fastio;
 
     int tc, n, k, a, b;
+    string op;
     cin >> tc;
 
     while (tc--)
     {
         cin >> n >> k;
+
+        curr_cc = 0;
+        vector<pair<int, int>> edgeList;
+
         for (int i = 1; i <= n; i++)
             ar[i].clear(), visited[i] = 0;
+
+        for (int i = 1; i <= k; i++)
+        {
+            cin >> a >> op >> b;
+
+            if (op == "=")
+                ar[a].push_back(b), ar[b].push_back(a);
+            else
+                edgeList.push_back({a, b}); // inserting as a pair
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (!visited[i])
+            {
+                curr_cc++;
+                dfs(i);
+            }
+        }
+
+        bool flag = true;
+        for (int i = 0; i < edgeList.size(); i++)
+        {
+            a = edgeList[i].first;
+            b = edgeList[i].second;
+
+            if (cc[a] == cc[b])
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
 }
+
+// Sample Input
+// 2
+// 2 2
+// 1 = 2
+// 1 != 2
+// 3 2
+// 1 = 2
+// 2 != 3
+// Sample Output
+// NO
+// YES
