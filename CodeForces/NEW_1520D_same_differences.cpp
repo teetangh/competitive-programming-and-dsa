af6f3fd4adefd3a5978ef44a1fec8c8bee60ca75
Kaustav Ghosh <teetangh@gmail.com>
2021-07-31 21:36:48 +0530

    working on NEW_1520D_same_differences codeforces

---
+++ CodeForces/NEW_1520D_same_differences.cpp
@@ -0,0 +1,45 @@
+// Includes
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Defines
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+
+// Typedefs
+typedef long long ll;
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef unsigned long long int ulli;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("xinput.txt", "r", stdin);   // for getting input from xinput.txt
+    freopen("xoutput.txt", "w", stdout); // for writing output to xoutput.txt
+#endif
+
+    fastio;
+
+    int tc;
+    cin >> tc;
+    // cout << tc;
+
+    while (tc--)
+    {
+        int n, temp;
+        cin >> n;
+
+        unordered_map<int, int> ump;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> temp;
+            ump[temp]++;
+
+            
+        }
+    }
+}
