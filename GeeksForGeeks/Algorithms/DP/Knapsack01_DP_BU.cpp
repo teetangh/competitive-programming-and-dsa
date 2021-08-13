1cd648b98a4b3741d297db70add1ef08ecaba425
Kaustav Ghosh <teetangh@gmail.com>
2021-08-13 10:08:10 +0530

    added knapsack 01 DP - (TD + BU) gfg

--- GeeksForGeeks/Algorithms/DP/Knapsack01_DP_BU.cpp
+++ GeeksForGeeks/Algorithms/DP/Knapsack01_DP_BU.cpp
@@ -7,30 +7,33 @@ using namespace std;
 class Solution
 {
 
-    int knapSack(int W, int wt[], int val[], int n, vector<vector<int>> &dp)
-    {
-        if (n == 0 || W == 0)
-            return dp[n][W] = 0;
-
-        if (dp[n][W] != -1)
-            return dp[n][W];
-
-        if (wt[n - 1] <= W)
-            return dp[n][W] = max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1, dp),
-                                  knapSack(W, wt, val, n - 1, dp));
-        else
-            return dp[n][W] = knapSack(W, wt, val, n - 1, dp);
-    }
-
 public:
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n)
     {
         // Your code here
         vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
-        return knapSack(W, wt, val, n, dp);
+
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j < W + 1; j++)
+            {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+
+                else if (wt[i - 1] <= j)
+                    dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]],
+                                   dp[i - 1][j]);
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        int answer = dp[n][W];
+        dp.clear();
+        return answer;
     }
-};  
+};
 
 // { Driver Code Starts.
 
