8b0149a60c60da5734c164d20bf8eafd3d5c6d85
Kaustav Ghosh <teetangh@gmail.com>
2021-08-13 10:23:56 +0530

    added Equal Sum Partition  DP - (BU) gfg

--- GeeksForGeeks/Algorithms/DP/EqualSumPartition_DP_BU.cpp
+++ GeeksForGeeks/Algorithms/DP/EqualSumPartition_DP_BU.cpp
@@ -9,42 +9,49 @@ using namespace std;
 
 class Solution
 {
-public:
-    int equalPartition(int N, int arr[])
+    bool isSubsetSum(int N, int arr[], int sum)
     {
         // code here
-        int sum = std::accumulate(arr, arr + N, 0);
-        int target;
+        vector<vector<int>> dp(N + 1, vector<int>(sum + 1, -1));
 
-        if (sum % 2 != 0)
-            return 0;
-        else
-            target = sum / 2;
-
-        vector<vector<int>> dp(N + 1, vector<int>(target + 1, 0));
         for (int i = 0; i < N + 1; i++)
         {
-            for (int j = 0; j < target + 1; j++)
+            for (int j = 0; j < sum + 1; j++)
             {
-                if (i == 0)
-                    dp[i][j] = 0;
-                if (j == 0)
-                    dp[i][j] = 1;
-            }
-        }
+                if (i == 0 && j == 0)
+                    dp[i][j] = true;
+                else if (i == 0)
+                    dp[i][j] = false;
+                else if (j == 0)
+                    dp[i][j] = true;
 
-        for (int i = 1; i < N + 1; i++)
-        {
-            for (int j = 1; j < target + 1; j++)
-            {
-                if (arr[i - 1] <= j)
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+                else if (arr[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
                 else
                     dp[i][j] = dp[i - 1][j];
             }
         }
 
-        return dp[N][target];
+        int answer = dp[N][sum];
+        dp.clear();
+        return answer;
+    }
+
+public:
+    int equalPartition(int N, int arr[])
+    {
+        // code here
+        int sum = 0;
+        for (int i = 0; i < N; i++)
+            sum += arr[i];
+
+        if (sum % 2 != 0)
+            return false;
+        else
+        {
+            int target = (sum / 2);
+            return isSubsetSum(N, arr, target);
+        }
     }
 };
 
